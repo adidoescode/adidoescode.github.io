a0d0a1af1ebf8cacf896b7d06b86caae422a62f8
Adam Sjögren <adam.ch.sjogren@gmail.com>
2023-11-13 18:54:46 +0100

    Har lagt grunden för koden, fetchat data och skapat funktion för displayen av kanalerna

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Denna fil ska innehålla din lösning till uppgiften (moment 5).
+// Lösning av moment 5, Sveriges Radio API löst av Adam Sjögren
 
 "use strict";
 
@@ -8,3 +8,33 @@ document.getElementById("player").style.display = "none";      // Radera denna r
 document.getElementById("shownumrows").style.display = "none"; // Radera denna rad för att visa antal träffar
 
 /* Här under börjar du skriva din JavaScript-kod */
+
+let mainNavEl = document.getElementById("mainnav");
+let mainNavListEl = document.getElementById("mainnavlist")
+
+/* Funktion som aktiveras när man laddar upp hemsidan*/
+
+window.onload = init;
+
+function init() {
+    getChannels();
+}
+
+function getChannels() {
+    const url = "http://api.sr.se/api/v2/channels/?format=json";
+
+    //Fetchar API
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayChannels(data))
+        .catch(error => console.log(error));
+}
+
+
+// Onclick visar programtablå för aktuella kanalen med start på kanalen fram till midnatt
+
+
+
+// 
+

24eb1166f5ed8ffd853cfcb59c749e6f6c8c8c6a
Adam Sjögren <adam.ch.sjogren@gmail.com>
2023-12-07 17:48:29 +0100

    Nu är jag klar med programmet efter många om och men, fixade timezoner och så att tablån bara visas för de episoder som inte redan har sänts.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,6 @@
 
 "use strict";
 
-/*  Delar till ej obligatorisk funktionalitet, som kan ge poäng för högre betyg
-*   Radera rader för funktioner du vill visa på webbsidan. */
-document.getElementById("player").style.display = "none";      // Radera denna rad för att visa musikspelare
-document.getElementById("shownumrows").style.display = "none"; // Radera denna rad för att visa antal träffar
-
 // Funktion som aktiveras när man laddar upp hemsidan
 
 window.onload = init;
@@ -26,37 +21,41 @@ function getChannels() {
         .catch(error => console.log(error));
 }
 
+//Funktion som med hjälp av datan som har hämtats av API:n skriver ut kanalerna i li format till en ul som sedan radas upp på vänstersidan av index.html filen
+
 function displayChannels(channels) {
 
     channels.forEach(channel => {
         const navUlEl = document.getElementById("mainnavlist")
 
 
-        //Utskrift ska se ut som liknande: <li>test</li>
+        //Utskrift ska se ut som liknande: <ul><li>kanal1</li><li>kanal2</li></ul> osv.
         let newLiEl = document.createElement("li");
         let newLiText = document.createTextNode(channel.name);
 
+        // Lägger till eventListener till när muspekaren är över en av kanalerna, då ska funktionen displayChannelDesc köras
         newLiEl.addEventListener("click", function () {
             getChannelDesc(channel.id);
         });
 
-
+        // Appendar både texten (utskrift av kanal i textformat) och li elementet till unordered listan.
         newLiEl.appendChild(newLiText);
         navUlEl.appendChild(newLiEl);
 
     });
 }
 
-// Lägger till eventListener till när muspekaren är över en av kanalerna, då ska funktionen displayChannelsDesc köras
 
 
+// Funktion som med hjälp av id:n som kommer när man aktiverar eventlistenern tar fram tablån för kanalen som har blivit klickad på.
 function getChannelDesc(id) {
-    const url = 'https://api.sr.se/api/v2/scheduledepisodes?format=json&channelid=' + id;
+    const url = 'https://api.sr.se/api/v2/scheduledepisodes?format=json&channelid=' + id + '&size=999';
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
             displayChannelDesc(data.schedule);
+            console.log(url);
         })
         .catch(error => console.log(error));
 }
@@ -66,30 +65,66 @@ function displayChannelDesc(schedule) {
 
     articleEl.innerHTML = "";
 
-    schedule.forEach(item => {
-        let newArticleEl = document.createElement("article");
-        let newTitleEl = document.createElement("h3");
-        let newTitleText = document.createTextNode(item.title);
-        newTitleEl.appendChild(newTitleText);
+    const currentTime = new Date();
+    if (schedule.length > 0) {
+        schedule.forEach(item => {
 
-        let newTimeEl = document.createElement("h5");
-        let newTimeText = document.createTextNode(item.starttimeutc + ' - ' + item.endtimeutc);
-        newTimeEl.appendChild(newTimeText);
+            const episodeTime = new Date(parseInt(item.starttimeutc.substr(6)));
+
+            if (episodeTime > currentTime) {
+
+                let newArticleEl = document.createElement("article");
+                let newTitleEl = document.createElement("h3");
+                let newTitleText = document.createTextNode(item.title);
+                newTitleEl.appendChild(newTitleText);
+
+                let newTimeEl = document.createElement("h5");
+                let newTimeText = document.createTextNode(convertTime(item.starttimeutc, item.endtimeutc));
+                newTimeEl.appendChild(newTimeText);
+
+                let newDescriptionEl = document.createElement("p");
+                let newDescriptionText = document.createTextNode(item.description);
+                newDescriptionEl.appendChild(newDescriptionText);
+
+                newArticleEl.appendChild(newTitleEl);
+                newArticleEl.appendChild(newTimeEl);
+                newArticleEl.appendChild(newDescriptionEl);
+
+                articleEl.appendChild(newArticleEl);
+            }
+        });
+    }
+    else {
+        articleEl.innerHTML = "<p>Denna radiokanalen sänder för nuvarande inga episoder.</p>";
+    }
+
+}
+
+function convertTime(timeStringStart, timeStringEnd) {
+    let tempTimeStart = new Date(parseInt(timeStringStart.substr(6)));
+    let tempTimeEnd = new Date(parseInt(timeStringEnd.substr(6)));
+
+    let hoursStart = tempTimeStart.getHours();
+    let minutesStart = tempTimeStart.getMinutes();
+
+    if (hoursStart < 10) { hoursStart = "0" + hoursStart; }
+    if (minutesStart < 10) { minutesStart = "0" + minutesStart; }
+
+    let fixedTimeStart = hoursStart + ":" + minutesStart;
 
 
+    let hoursEnd = tempTimeEnd.getHours();
+    let minutesEnd = tempTimeEnd.getMinutes();
 
-        let newDescriptionEl = document.createElement("p");
-        let newDescriptionText = document.createTextNode(item.description);
-        newDescriptionEl.appendChild(newDescriptionText);
+    if (hoursEnd < 10) { hoursEnd = "0" + hoursEnd; }
+    if (minutesEnd < 10) { minutesEnd = "0" + minutesEnd; }
 
-        newArticleEl.appendChild(newTitleEl);
-        newArticleEl.appendChild(newTimeEl);
-        newArticleEl.appendChild(newDescriptionEl);
-
-        articleEl.appendChild(newArticleEl);
-    })
+    let fixedTimeEnd = hoursEnd + ":" + minutesEnd;
 
 
+    let fixedTime = fixedTimeStart + " - " + fixedTimeEnd;
+
+    return fixedTime;
 }
 
 
